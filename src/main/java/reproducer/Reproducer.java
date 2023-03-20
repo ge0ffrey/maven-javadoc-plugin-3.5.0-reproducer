c29194169c50755e5e4278127a72cb2c277cccc9
Geoffrey De Smet <gds.geoffrey.de.smet@gmail.com>
2023-03-20 15:08:19 +0100

    reproducer

---
+++ src/main/java/reproducer/Reproducer.java
@@ -0,0 +1,17 @@
+package reproducer;
+
+import org.openrewrite.gradle.ChangeDependencyArtifactId;
+
+/**
+ * This is the class javadoc
+ */
+public class Reproducer {
+
+    /**
+     * This is a method javadoc.
+     */
+    public void myMethod() {
+        System.out.println(ChangeDependencyArtifactId.class);
+    }
+
+}
